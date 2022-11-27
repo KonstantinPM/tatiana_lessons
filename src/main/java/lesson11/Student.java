fc7b4d0459718054d11f56b9a52bfee72dc60be2
Tanya Serbinenko <tanyaserbinenko1996@gmail.com>
2022-11-27 12:16:27 +0200

    init

---
+++ src/main/java/lesson11/Student.java
@@ -0,0 +1,10 @@
+package lesson11;
+
+//import lombok.Data;
+
+//@Data
+/*public class Student {
+    private int id;
+    private String name;
+
+}*/
---
+++ src/main/java/lesson11/StudentRun.java
@@ -0,0 +1,10 @@
+package lesson11;
+
+public class StudentRun {
+    public static void main(String[] args) {
+     //   Student st = new Student();
+       // st.setId(6);
+        //st.setName("Vlad");
+        //System.out.println(st);
+    }
+}
