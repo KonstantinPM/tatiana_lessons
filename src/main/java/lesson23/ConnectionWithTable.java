b2355bab529c8dc3c480dd6f0f2ec6eb09c52a9f
Tanya Serbinenko <tanyaserbinenko1996@gmail.com>
2023-01-26 09:39:56 +0200

    initian

--- src/main/java/lesson23/ConnectionWithTable.java
+++ src/main/java/lesson23/ConnectionWithTable.java
@@ -7,7 +7,7 @@ public class ConnectionWithTable {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        //Methods.addStudent(new Student(4, "Alina Gagarina"));
+        Methods.addStudent(new Student(4, "Alina Gagarina"));
         Methods.showStudents().forEach(System.out::println);
         System.out.println(Methods.getById(1));
         System.out.println(Methods.getByName("Tanya Serbinenko"));
