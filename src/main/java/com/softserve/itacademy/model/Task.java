65b8444d37ddee053d1c74c00cf68f703ca7069b
Maryna <belenckaia.marina@gmail.com>
2023-05-31 17:49:30 +0300

    State & Task

--- src/main/java/com/softserve/itacademy/model/Task.java
+++ src/main/java/com/softserve/itacademy/model/Task.java
@@ -1,5 +1,96 @@
 package com.softserve.itacademy.model;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tasks")
 public class Task {
-//
+
+    @Id
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "task_sequence"),
+                    @Parameter(name = "initial_value", value = "10"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
+
+    private long id;
+
+    @NotBlank(message = "The taskName cannot be empty")
+    @Column(nullable = false)
+    @NotNull
+    @Size(min = 3, max = 200)
+    private String name;
+
+    @NotBlank(message = "The priority cannot be empty")
+    @Column(nullable = false)
+    private Priority priority;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "state_id")
+    private State state;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "todo_id")
+    private ToDo todo;
+
+    public Task() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public ToDo getTodo() {
+        return todo;
+    }
+
+    public void setTodo(ToDo todo) {
+        this.todo = todo;
+    }
+
+    @Override
+    public String toString() {
+        return "Task {" +
+                "id = " + id +
+                ", name = '" + name + '\'' +
+                ", priority = " + priority +
+                ", state_id = " + state +
+                ", todo_id = " + todo +
+                '}';
+    }
 }
